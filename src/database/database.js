05988897fdee038a74b2b9098f5f593192917b91
wellingtonlopes <ookami.seta@gmail.com>
2020-10-18 16:15:37 -0400

    Fix database date entry on appointments

--- src/database/database.js
+++ src/database/database.js
@@ -99,7 +99,7 @@ export let appointments = [
   name: 'Winston Churchill',
   doctor: 'Dr. Lúcio Correia dos Santos',
   field: 'Pediatrics',
-  date: new Date('2021-1-30T015:00:00'),
+  date: new Date('2021-01-30T15:00:00'),
   time: "15:00"
 }
 ]
